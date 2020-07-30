7563ad848155890fbb624fea4f033e3a700610fa
Jess Gray <43485987+jgray314@users.noreply.github.com>
2020-07-29 21:20:33 -0700

    Add caesar cipher and clean up a bit

--- cipher.go
+++ cipher.go
@@ -6,14 +6,13 @@ import (
 )
 
 // Normalizes a string to characters in the the set [a-z]
+// Common step for classical crypto
 func Normalize(s string) string {
 	s = strings.ToLower(s)
 	r := ""
 	for _, a := range s {
 		if a >= 'a' && a < 'z' {
 			r = r + string(a)
-		} else {
-			r = r + " "
 		}
 	}
 	return r
@@ -26,34 +25,34 @@ func CondAppend(s string, cond bool, t rune, f rune) string {
 	return s + string(f)
 }
 
-
 // Funcation ideas from http://practicalcryptography.com/ciphers/classical-era/
 func Atbash(s string) string {
 	r := ""
 	for _, a := range s {
-		r = CondAppend(r, a == ' ', ' ', 'z'-a+'a')
+		r = r + string('z'-a+'a')
+	}
+	return r
+}
+
+func Caesar(s string, shift int) string {
+	r := ""
+	shift = shift % 26 // More adjustments needed for negative shifts
+	for _, a := range s {
+		r = CondAppend(r, a+rune(shift) > 'z', a+rune(shift)-26, a+rune(shift))
 	}
 	return r
 }
 
 func Rot13(s string) string {
-	r := ""
-	for _, a := range s {
-		if a == ' ' {
-			r = r + " "
-		} else {
-			r = CondAppend(r, a > 'm', a-13, a+13)
-		}
-	}
-	return r
+	return Caesar(s, 13)
 }
-
-
 
 func main() {
 	str := "Hello, playground"
 	fmt.Println(str)
 	str = Normalize(str)
+	fmt.Println(str)
 	fmt.Println(Atbash(str))
+	fmt.Println(Caesar(str, 1))
 	fmt.Println(Rot13(str))
 }
