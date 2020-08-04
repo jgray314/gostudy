ae1bf206497024af6550c1a3b02e140b92193f63
Jess Gray <43485987+jgray314@users.noreply.github.com>
2020-08-03 20:12:15 -0700

    Time to create an interface

--- cipher.go
+++ cipher.go
@@ -5,9 +5,15 @@ import (
 	"strings"
 )
 
+type cipher interface {
+	name() string
+	encrypt(string) string
+	decrypt(string) string
+}
+
 // Normalizes a string to characters in the the set [a-z]
 // Common step for classical crypto
-func Normalize(s string) string {
+func normalize(s string) string {
 	s = strings.ToLower(s)
 	r := ""
 	for _, a := range s {
@@ -18,7 +24,13 @@ func Normalize(s string) string {
 	return r
 }
 
-func CondAppend(s string, cond bool, t rune, f rune) string {
+func print_demo(c cipher, s string) {
+	n := normalize(s)
+	e := c.encrypt(n)
+	fmt.Printf("Cypher: %s\tOriginal: %s\tNormalized: %s\tEncrypt: %s\tDecrypt: %s\n", c.name(), s, n, e, c.decrypt(e))
+}
+
+func cond_append(s string, cond bool, t rune, f rune) string {
 	if cond {
 		return s + string(t)
 	}
@@ -26,7 +38,12 @@ func CondAppend(s string, cond bool, t rune, f rune) string {
 }
 
 // Funcation ideas from http://practicalcryptography.com/ciphers/classical-era/
-func Atbash(s string) string {
+type atbash struct {}
+
+func (a atbash) name() string {
+	return "atbash"
+}
+func (a atbash) encrypt(s string) string {
 	r := ""
 	for _, a := range s {
 		r = r + string('z'-a+'a')
@@ -34,25 +51,55 @@ func Atbash(s string) string {
 	return r
 }
 
-func Caesar(s string, shift int) string {
+func (a atbash) decrypt(s string) string {
+	return a.encrypt(s)
+}
+
+func shift(s string, shift int) string {
 	r := ""
-	shift = shift % 26 // More adjustments needed for negative shifts
+	for shift < 0 {
+		shift += 26
+	}	
+	shift = shift % 26
 	for _, a := range s {
-		r = CondAppend(r, a+rune(shift) > 'z', a+rune(shift)-26, a+rune(shift))
+		r = cond_append(r, a+rune(shift) > 'z', a+rune(shift)-26, a+rune(shift))
 	}
 	return r
 }
 
-func Rot13(s string) string {
-	return Caesar(s, 13)
+type caesar struct {
+	shift int
+}
+
+func (c caesar) name() string {
+	return "ceasar"
+}
+
+func (c caesar) encrypt(s string) string {
+	return shift(s, c.shift)
+}
+
+func (c caesar) decrypt(s string) string {
+	return shift(s, 26 - c.shift)
+}
+
+type rot13 struct {}
+
+func (r rot13) name() string {
+	return "rot13"
+}
+
+func (r rot13) encrypt(s string) string {
+	return shift(s, 13)
+}
+
+func (r rot13) decrypt(s string) string {
+	return shift(s, 13)
 }
 
 func main() {
-	str := "Hello, playground"
-	fmt.Println(str)
-	str = Normalize(str)
-	fmt.Println(str)
-	fmt.Println(Atbash(str))
-	fmt.Println(Caesar(str, 1))
-	fmt.Println(Rot13(str))
+	s := "Hello, playground"
+	print_demo(atbash{}, s)
+	print_demo(caesar{shift: 3},s)
+	print_demo(rot13{}, s)
 }
